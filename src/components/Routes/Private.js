717198cff1ffb5a4113b52fbcc78900187d52986
pawan negi <negi61332@gmail.com>
2025-09-13 18:12:22 +0530

    url change

--- src/components/Routes/Private.js
+++ src/components/Routes/Private.js
@@ -3,7 +3,7 @@ import { useAuth } from "../../context/auth";
 import { Outlet } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../Spinner";
-
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export default function PrivateRoute() {
   const [ok, setOk] = useState(false);
@@ -12,7 +12,7 @@ export default function PrivateRoute() {
   useEffect(() => {
     const authCheck = async () => {
       try {
-        const res = await axios.post("/api/v1/auth/user-auth");
+        const res = await axios.post(`${apiUrl}/api/v1/auth/user-auth`);
         if (res.data.ok) { 
           setOk(true);
         } else {
